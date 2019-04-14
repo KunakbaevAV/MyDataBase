77bc383d773942f7b1b04bd25210fa424c1bb209
Artem <kunakbaevav@yandex.ru>
2019-04-14 15:13:49 +0500

    Домашнее задание к 3 уроку

---
+++ lesson3_shtat.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.11, for Win64 (x86_64)
+--
+-- Host: 192.168.1.11    Database: lesson3
+-- ------------------------------------------------------
+-- Server version	8.0.13
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `shtat`
+--
+
+DROP TABLE IF EXISTS `shtat`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `shtat` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lastname` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `dept` int(11) DEFAULT NULL,
+  `position` varchar(40) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `salary` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `shtat`
+--
+
+LOCK TABLES `shtat` WRITE;
+/*!40000 ALTER TABLE `shtat` DISABLE KEYS */;
+INSERT INTO `shtat` VALUES (1,'Иван','Васильков',1,NULL,50000),(2,'Василий','Чумазый',1,NULL,46000),(3,'Петр','Красилов',2,NULL,45000),(4,'Макар','Звонкий',3,NULL,55000),(5,'Мария','Ленивая',5,'Уборщица',35000),(6,'Артем','Кунакбаев',6,'Ведущий программист',95000),(9,'Ирина','Кудрявая',7,'Делопроизводитель',35000),(10,'Дмитрий','Веселый',6,'Инженер',111000),(11,'Антон','Бескровный',6,NULL,105000);
+/*!40000 ALTER TABLE `shtat` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-04-14 13:18:45
